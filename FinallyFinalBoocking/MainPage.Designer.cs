032a116e0194a848376c5315455d42bae72c8405
Orest Hevorhian <o.hevorhyan@gmail.com>
2024-12-10 17:27:28 +0100

    Changed the groupbox text

--- FinallyFinalBoocking/MainPage.Designer.cs
+++ FinallyFinalBoocking/MainPage.Designer.cs
@@ -92,7 +92,7 @@
             groupBox1.Size = new Size(1062, 135);
             groupBox1.TabIndex = 3;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            groupBox1.Text = "Account details";
             // 
             // Exitbtn2
             // 
